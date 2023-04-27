d636b0ed9a4700a5a3d64d4e7848218336f677f7
JuanConde27 <condejuan27@gmail.com>
2023-04-27 12:54:39 -0500

    updated

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
-const table = document.querySelector('table');
+const form = document.getElementById('form');
+const searchInput = document.getElementById('search');
+const tbody = document.querySelector('tbody');
+let data; 
 
 fetch('https://api.binance.com/api/v3/ticker/price')
 .then(response => response.json())
-.then(data => {
+.then(responseData => {
+    data = responseData; // Asignar el valor de responseData a la variable data
     console.log(data);
     data.forEach(element => {
-        table.innerHTML += `
+        tbody.innerHTML += `
         <tr>
             <td>${element.symbol}</td>
             <td>${element.price}</td>
         </tr>
+        `;
+    });
+})
+.catch(error => {
+    console.log('Error:', error);
+});
+
+form.addEventListener('input', (event) => {
+    event.preventDefault(); 
+
+    const searchValue = searchInput.value.toUpperCase(); // Obtener el valor del campo de búsqueda en mayúsculas
+    const filteredData = data.filter((item) => item.symbol.toUpperCase().includes(searchValue) || item.price.toUpperCase().includes(searchValue)); // Filtrar los datos
+
+    tbody.innerHTML = ''; 
+
+    filteredData.forEach((element) => {
+        tbody.innerHTML += `
+            <tr>
+                <td>${element.symbol}</td>
+                <td>${element.price}</td>
+            </tr>
         `;
     });
 });
